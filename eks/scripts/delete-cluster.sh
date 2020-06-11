00e480bf2099e09db3e1921b755b0bb22219b962
RajendraBobade <rajubobade@gmail.com>
2020-06-11 15:03:21 +0400

    first commit

---
+++ eks/scripts/delete-cluster.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/env bash
+# Destroy Cluster
+
+#- VARIABLES -#
+CLUSTERID=$1
+
+if [ -z "${CLUSTERID}" ]; then
+  echo -n "Please enter a name for your cluster and press [ENTER]":
+  read CLUSTERID
+fi
+
+eksctl delete cluster $CLUSTERID -w
